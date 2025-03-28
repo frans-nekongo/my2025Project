9e9bc10fb193988d412a47069baeead75cdd4483
zxkgeno <83214738+frans-nekongo@users.noreply.github.com>
2025-03-28 23:36:43 +0200

    Update TeamService.kt

--- Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
+++ Homework-1/aliasServer/src/main/kotlin/jetbrains/kotlin/course/alias/team/TeamService.kt
@@ -1,9 +1,34 @@
 package jetbrains.kotlin.course.alias.team
 
+import jetbrains.kotlin.course.alias.util.Identifier
+import jetbrains.kotlin.course.alias.util.IdentifierFactory
 import org.springframework.stereotype.Service
 
 @Service
 class TeamService {
 
-    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> = TODO("Not implemented yet")
+    // 1. Added identifierFactory property
+    val identifierFactory = IdentifierFactory()
+
+    // 2. Added companion object with teamsStorage
+    companion object {
+        val teamsStorage: MutableMap<Identifier, Team> = mutableMapOf()
+    }
+
+    // 3. Implemented the generateTeamsForOneRound method
+    fun generateTeamsForOneRound(teamsNumber: Int): List<Team> {
+        // Basic validation
+        require(teamsNumber > 0) { "Number of teams must be positive" }
+
+        val newTeams = mutableListOf<Team>()
+        repeat(teamsNumber) {
+            val newId = identifierFactory.uniqueIdentifier()
+            val newTeam = Team(newId) // Creates team with new ID and default points (0)
+            teamsStorage[newId] = newTeam // Store the team in the central storage
+            newTeams.add(newTeam) // Add to the list for this round
+        }
+        return newTeams // Return the list of teams created in this call
+    }
+
+    // Any other existing methods in TeamService would remain here
 }
